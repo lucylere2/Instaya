43c06fa089740c27bd29e2c1b6c3459aa14c2962
Luz Elena Ramos <luzramos50@gmail.com>
2022-11-20 11:17:58 -0500

    Se agrega barra de navegacion

--- src/App.jsx
+++ src/App.jsx
@@ -11,31 +11,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './assets/Componentes/Footer'
 import React from 'react'
 import './assets/Componentes/Footer.css';
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Header from './assets/Componentes/Header'
 
 function App() {
   
   return (
     <BrowserRouter>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/quienesSomos">Quienes Somos </Link>
-          </li>
-          <li>
-            <Link to="/servicios">Servicios </Link>
-          </li>
-          <li>
-            <Link to="/registrarse">Registrarse </Link>
-          </li>
-          <li>
-            <Link to="/solicitudenvio">Solicitar Envio</Link>
-          </li>
-        </ul>
-      </nav>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            
+            <Nav.Link as={Link} to='/quienessomos'>Quienes </Nav.Link>
+            <Nav.Link as={Link} to='/servicios'>Servicios</Nav.Link>
+            <Nav.Link as={Link} to='/registrarse'>Registrarse</Nav.Link>
+            <Nav.Link as={Link} to='/solicitudenvio'>Solicitud de Envio</Nav.Link>
+           </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
       
       <Routes>
-        <Route path='/' element={<h1>Inicio</h1>}/>
+        <Route path='/' element={<h1> </h1>}/>
         <Route path='/registrarse' element={<Registrarse/>}/>
         <Route path='/solicitudenvio' element={<Solicitudenvio/>}/>
         <Route path='/servicios' element={<Servicios/>}/>
---
+++ src/assets/Componentes/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return(
+        <nav>
+            <div className="nav-wrapper ligth-blue darken=2">
+                <a href="#!" className="brand-logo">Titulo</a>
+            </div>
+        </nav>
+    )
+}
